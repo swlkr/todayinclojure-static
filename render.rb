9612d9e02fb3b3f5c726e6b3d0bb832bfd166e7d
Sean Walker <swlkr.rbl@gmail.com>
2020-02-29 22:51:32 -0700

    Put the time back in there

--- render.rb
+++ render.rb
@@ -6,13 +6,14 @@ require 'json'
 hn_posts = JSON.parse File.read('hn.json')
 reddit_posts = JSON.parse File.read('reddit.json')
 
-posts = {
-  posts: hn_posts.concat(reddit_posts).sort_by { |p| p['created-at'] }.reverse
+data = {
+  posts: hn_posts.concat(reddit_posts).sort_by { |p| p['created-at'] }.reverse,
+  today: Time.now.utc.strftime('%b %d, %Y')
 }
 
 template = File.read 'index.mustache'
 
-File.write 'index.html', Mustache.render(template, posts)
+File.write 'index.html', Mustache.render(template, data)
 
 File.delete 'hn.json'
 File.delete 'reddit.json'
