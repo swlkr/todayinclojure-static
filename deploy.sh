4bfd18a4902ba52f6253cf0794d6ec70ae168f45
Sean Walker <swlkr.rbl@gmail.com>
2020-03-04 15:16:00 -0700

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git checkout source && make build && git checkout master && mv dist/* . && git add -A && git commit -a -m "Update" && git push origin master
+git checkout source && make build && git checkout master && mv dist/* . && git add -A && git commit -m \"Update\" && git push origin master
